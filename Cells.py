c059e101113e32b49f2648093aee5cbab3b9c0f7
Askari Iqbal <gb.lespaul12@gmail.com>
2019-10-03 23:05:04 +0300

    Parsed for clues and cells

---
+++ Cells.py
@@ -0,0 +1,20 @@
+# LIST OF CELLS THAT HAVE NUMBER AND CHARACTERS
+# IF IT IS A BLACK CELL THE CHARACTER IS BLACK
+# IF THERE IS NO NUMBER THEN NUMBER IS BLANK
+# NUMBERS ARE CHARACTERS
+# YOU CAN USE isBlack() TO SEE IF A CELL IS A BLACK CELL
+# YOU CAN USE hasNum() TO SEE IF A CELL HAS A BLACK CELL
+class Cells:
+
+    def __init__(self , number, character):
+
+        self.n = number
+        self.c = character
+
+    def getChar(self):
+
+        return self.c
+
+    def getNum(self):
+
+        return  self.n
--- ParseInfo.py
+++ ParseInfo.py
@@ -1,51 +1,86 @@
 from bs4 import BeautifulSoup as bs
+from Cells import Cells
 
 html = ""
 black_class_Name = "Cell-block--1oNaD"
 white_class_name = "Cell-cell--1p4gH"
 
-white_cells = []
-black_cells = []
+clues_class_name = "Layout-clueLists--10_Xl"
 
 with open("html.txt" , "r") as html_file:
     html = html_file.read()
 
 parsed_html = bs(html)
 
-def find_info_cells(parsed_html):
-    for g in parsed_html.find_all("g"):
+def getGrid():
+    cells_array = []
+
+    def find_info_cells(parsed_html):
+        for g in parsed_html.find_all("g"):
+
+            try:
+                if (g['data-group'] == "cells"):
+                    return g.find_all("g")
+
+            except Exception as e:
+                pass
+
+    info_cells = find_info_cells(parsed_html)
+
+    for cell in info_cells:
 
         try:
-            if (g['data-group'] == "cells"):
-                return  g.find_all("g")
+
+            rect = cell.find("rect")
+            character = ""
+            number = ""
+
+            if (rect["class"] == [black_class_Name]):
+                character = "Black"
+            else:
+
+                texts = cell.find_all("text")
+                for a_text in texts:
+
+                    if (a_text["text-anchor"] == "start"):
+                        number = a_text.string
+
+                    if (a_text["text-anchor"] == "middle"):
+                        character = a_text.string
+
+            cells_array.append(Cells(number=number, character=character))
 
         except Exception as e:
             pass
 
-info_cells = find_info_cells(parsed_html)
+    return cells_array
 
-for cell in info_cells:
+cells_array = getGrid()
 
-    try:
+def getClues():
+    sections = parsed_html.find_all("section")
+    a_clues = []
+    d_clues = []
 
-        rect = cell.find("rect")
+    for a_section in sections:
 
-        if (rect["class"] == [black_class_Name]):
+        def getClue(clue_list):
 
-            black_cells.append(cell)
+            new_clue_list = []
+            for clue in clue_list:
+                new_clue_list.append(clue.find_all("span")[1].string)
+            return new_clue_list
 
-        else:
+        # Section for clues section
+        if (a_section["class"] == [clues_class_name]):
+            # Search for accross clues
+            across_clues = a_section.find("div").find_all("li")
+            a_clues = getClue(across_clues)
 
-            white_cells.append((cell))
+            # Search for down clues
+            down_clues = a_section.find_all("div")[1].find_all("li")
+            d_clues = getClue(down_clues)
 
-    except Exception as e:
-        pas
+    return a_clues , d_clues
 
-print (len(white_cells))
-print (len(black_cells))
-
-class Cells:
-
-    def __init__(self):
-
-        pass
+a_clues , d_clues = getClues()
